5d9bfd82447389da99d8f7178ce02403d1b0d01d
Vishal Naik <vishal49naik49@gmail.com>
2023-05-15 16:36:02 -0400

    CFN template and Setup.py fixes

--- seedcode/sm-autopilot-seedcode-model-build/setup.py
+++ seedcode/sm-autopilot-seedcode-model-build/setup.py
@@ -5,7 +5,7 @@ import setuptools
 about = {}
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, "pipelines", "__version__.py")) as f:
-    exec(f.read(), about)
+    about = f.read()
 
 
 with open("README.md", "r") as f:
